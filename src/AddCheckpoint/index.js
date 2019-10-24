d94c829274a2d0de11ff333dded290e1d7b3dab5
Antto Hautamäki <antto_hautamaki@hotmail.com>
2019-10-24 08:14:14 +0000

    template for Add checkpoint added

---
+++ src/AddCheckpoint/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './../App.css';
+
+function AddCheckpoint() {
+  return (
+       <div class="row">
+     	  <div class="col-sm-4">
+	   <p>div</p>
+	 </div>
+
+          <div class="col-sm-4">                                                                                                   <p>div</p>
+                <p>div2</p>
+		</div> 
+	</div>
+  );
+}
+
+export default AddCheckpoint;
+
