95f933566801bfb3ec7e56eaa6f7812e475a855d
Antto Hautamäki <antto_hautamaki@hotmail.com>
2019-10-29 09:05:03 +0000

    added City scrollbar

--- src/DeleteRoute/index.js
+++ src/DeleteRoute/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import $ from "jquery";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button,Form} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './../App.css'
 
 class DeleteRoute extends React.Component {
@@ -12,16 +12,56 @@ class DeleteRoute extends React.Component {
       super(props);
  
            this.textInput = React.createRef();
+           this.deleteUusi= React.createRef();
+           this.city1= React.createRef();
+
            this.state = {
       			value: null,
-      			routes  :  [ ]
+      			routes  :  [ ],
+      			cities  :  [ ]
     		};
+    		this.getCities();
            
      
   }
+  componentDidMount()  {
+
+  
+  
+  }
+  getCities()   {
+  		
+  
+     	var settings = {
+			"crossDomain": true,
+  			"url": "http://192.168.99.100/api/oBackEnd/webresources/generic/cities",
+ 	 		"method": "GET",
+  			"headers": {
+    			"Content-Type": "application/json",
+    			"Accept": "*/*"
+  			}}
+  			
+  			var t=this;
+
+			$.ajax(settings).done(function (response) {
+				alert(response[1])
+				let citiesName=[];
+				response.map(term=>citiesName.push(<option value={term} key="1" >{term}</option>) )
+				 t.setState({ cities  : citiesName});
+		
+				 /*const result = response.filter(route => route.city === "Tampere");
+				 let routesArray=[];
+				 result.map(route=>routesArray.push(<option value={route.id} key="1" >{route.name}</option>))
+				 t.setState({ routes  : routesArray});
+				 */
+			});
+				 			
+
+  }
   
     deleteRoute1()  {
-    	alert('je2s=' + this.textInput.current.value)
+    
+
     	
       	var settings = {
 			"crossDomain": true,
@@ -31,17 +71,15 @@ class DeleteRoute extends React.Component {
     			"Content-Type": "application/json",
     			"Accept": "*/*"
   			}}
+  			
   			var t=this;
 
 			$.ajax(settings).done(function (response) {
-				 const result = response.filter(route => route.city == "Tampere");
+				 const result = response.filter(route => route.city ===t.city1.current.value);
 				 let routesArray=[];
-				 result.map(route=>routesArray.push(	    <option value="1" key="1" >easy 1</option>))
-				
-			
+				 result.map(route=>routesArray.push(<option value={route.id} key="1" >{route.name}</option>))
 				 t.setState({ routes  : routesArray});
-				 alert(JSON.stringify(result ));
-			});
+				 			});
     }
    render()  {
    
@@ -52,12 +90,14 @@ class DeleteRoute extends React.Component {
           <div class="col-sm-4">
 			<p> Row1</p>
           </div>
+          
            <div class="col-sm-4">
-           <p>Filter Route by </p>
+          
+            <p>Filter Route by </p>
            
-            <select  onChange={this.deleteRoute1.bind(this)} ref={ this.textInput } class="browser-default custom-select">
+            <select  onChange={this.deleteRoute1.bind(this)} ref={ this.city1} class="browser-default custom-select">
         		      <option value="" selected disabled hidden>Select City</option>
-        		      <option value="Tampere" >Tampere</option>
+        		      {this.state.cities}
   			</select>
   			
   			 <select   class="browser-default custom-select">
@@ -65,17 +105,16 @@ class DeleteRoute extends React.Component {
         		   <option value="1" >Kauppi</option>
   			</select>
   			
-  		     <select   class="browser-default custom-select">
+  		     <select   class="browser-default custom-select"  ref={ this.deleteUusi} >
         		    <option value="" selected disabled hidden>Route</option>
         		    {this.state.routes}
         		
   			</select>
 
-
   				
   	       <Button variant="danger"  onClick={this.deleteRoute1.bind(this)} type="submit">
 	           Delete route
-  		</Button>
+  		   </Button>
 
 			<p> Row2</p>
 			
